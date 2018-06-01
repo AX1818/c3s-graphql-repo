01e3689f00bacb2e08f96d8730db2c423a5f8c71
Austin Xie <weiyu.xie@gmail.com>
2018-06-01 22:05:34 +1000

    fix typo

--- handler.js
+++ handler.js
@@ -6,10 +6,10 @@ const {
 } = require('graphql');
 
 const {schema} = require('./schema');
-const {resolovers} = require('./resolvers');
+const {resolvers} = require('./resolvers');
 
 module.exports.graphqlQuery = (event, context, callback) => {
-  graphql(schema, event.queryStringParameters.query, resolovers).then(
+  graphql(schema, event.queryStringParameters.query, resolvers).then(
     resp => callback(null, { statusCode: 200, body: JSON.stringify(resp) }),
     err => callback(err)
   );
