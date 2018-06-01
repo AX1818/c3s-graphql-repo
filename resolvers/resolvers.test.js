4054f67c4e5a65752133040db13b495a0f18f323
Austin Xie <weiyu.xie@gmail.com>
2018-06-01 21:36:12 +1000

    refined tests

--- resolvers/resolvers.test.js
+++ resolvers/resolvers.test.js
@@ -3,7 +3,7 @@ const { graphql, buildSchema } = require('graphql');
 const {schema} = require('../schema');
 const {resolvers} = require('./index');
 
-/*
+
 test('test resolving clothes and its schema', ()=> {
   graphql(schema, `query { 
     clothes(Ids: ["20130301_131132", "DSC_0380"]) {
@@ -18,7 +18,7 @@ test('test resolving clothes and its schema', ()=> {
     console.log('clothes: ', JSON.stringify(clothes));
   });
 });
-*/
+
 
 test('test get clothe by Id', ()=> {
   graphql(schema, `query { 
@@ -41,6 +41,34 @@ test('test get clothe by Id', ()=> {
   });
 });
 
+test('test Update Clothe', ()=> {
+  graphql(schema, `mutation { 
+    updateClothe(clothe: {Id: "20130301_131132", Code: "WIFE", State: AVAILABLE, Timestamp: "2018-06-01"}) {
+      Id
+      Type
+      Code
+      State
+      Timestamp
+    }
+  }`,
+  resolvers).then(clothe => {
+    console.log('Clothe: ', JSON.stringify(clothe));
+  });
+});
+
+
+test('test Update Tag', ()=> {
+  graphql(schema, `mutation { 
+    updateTag(tag: {Id: "d76e41ac-677f-424c-b348-0cbacdf15dae", Code: "AAAAA"}) {
+      Id
+      Type
+      Code
+    }
+  }`,
+  resolvers).then(tag => {
+    console.log('Tag: ', JSON.stringify(tag));
+  });
+});
 
 test('test get Tag by Id', ()=> {
   graphql(schema, `query { 
@@ -55,9 +83,6 @@ test('test get Tag by Id', ()=> {
   });
 });
 
-
-
-/*
 test('test Tagging Clothe by Ids', ()=> {
   graphql(schema, `mutation { 
     tagClothes(tagId: "d76e41ac-677f-424c-b348-0cbacdf15dae", clotheIds: ["20130301_131132", "DSC_0380"]) {
@@ -71,9 +96,7 @@ test('test Tagging Clothe by Ids', ()=> {
     console.log('ClotheTags: ', JSON.stringify(clothes));
   });
 });
-*/
 
-/*
 test('test adding Tag', ()=> {
   graphql(schema, `mutation { 
     addTag(Code: "T222") {
@@ -86,4 +109,3 @@ test('test adding Tag', ()=> {
     console.log('Tag: ', JSON.stringify(tag));
   });
 });
-*/
